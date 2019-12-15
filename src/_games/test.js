4dbfd462687a73af6741ab03f4dc4b76fdfeee5a
tx2z <tx2z@caosmental.com>
2019-12-15 03:30:48 +0100

    apply prettier to all files

--- src/_games/test.js
+++ src/_games/test.js
@@ -1,223 +1,215 @@
 const game = {
-// GAME SETTINGS
-	name: 'Test Game',
-	author: 'Jesús Pérez',
-	rows: 6,
-	cols: 8,
-	theme: 'default',
-	totalMines: 2,
-	totalTreasures: 2,
-	tiles: [
-		{
-			id: 0,
-			class: 'xxxx01',
-		},
-		{
-			id: 1,
-			class: 'xxxx01',
-		},
-		{
-			id: 2,
-			class: 'xxxx01',
-		},
-		{
-			id: 3,
-			class: 'xxxx01',
-		},
-		{
-			id: 4,
-			class: 'xxxx01',
-		},
-		{
-			id: 5,
-			class: 'xxxx01',
-		},
-		{
-			id: 6,
-			class: 'xxxx01',
-		},
-		{
-			id: 7,
-			class: 'xxxx01',
-		},
+  // GAME SETTINGS
+  name: 'Test Game',
+  author: 'Jesús Pérez',
+  rows: 6,
+  cols: 8,
+  theme: 'default',
+  totalMines: 2,
+  totalTreasures: 2,
+  tiles: [
+    {
+      id: 0,
+      class: 'xxxx01',
+    },
+    {
+      id: 1,
+      class: 'xxxx01',
+    },
+    {
+      id: 2,
+      class: 'xxxx01',
+    },
+    {
+      id: 3,
+      class: 'xxxx01',
+    },
+    {
+      id: 4,
+      class: 'xxxx01',
+    },
+    {
+      id: 5,
+      class: 'xxxx01',
+    },
+    {
+      id: 6,
+      class: 'xxxx01',
+    },
+    {
+      id: 7,
+      class: 'xxxx01',
+    },
 
+    {
+      id: 8,
+      class: 'xxxx01',
+    },
+    {
+      id: 9,
+      class: 'xxxx01',
+    },
+    {
+      id: 10,
+      block: true,
+      class: 'xxxx02',
+    },
+    {
+      id: 11,
+      class: 'xxxx01',
+    },
+    {
+      id: 12,
+      class: 'xxxx01',
+    },
+    {
+      id: 13,
+      class: 'xxxx01',
+    },
+    {
+      id: 14,
+      class: 'xxxx01',
+    },
+    {
+      id: 15,
+      class: 'xxxx01',
+    },
 
-		{
-			id: 8,
-			class: 'xxxx01',
-		},
-		{
-			id: 9,
-			class: 'xxxx01',
-		},
-		{
-			id: 10,
-			block: true,
-			class: 'xxxx02',
-		},
-		{
-			id: 11,
-			class: 'xxxx01',
-		},
-		{
-			id: 12,
-			class: 'xxxx01',
-		},
-		{
-			id: 13,
-			class: 'xxxx01',
-		},
-		{
-			id: 14,
-			class: 'xxxx01',
-		},
-		{
-			id: 15,
-			class: 'xxxx01',
-		},
+    {
+      id: 16,
+      class: 'xxxx01',
+    },
+    {
+      id: 17,
+      class: 'xxxx01',
+    },
+    {
+      id: 18,
+      class: 'xxxx01',
+    },
+    {
+      id: 19,
+      class: 'xxxx01',
+    },
+    {
+      id: 20,
+      class: 'xxxx01',
+    },
+    {
+      id: 21,
+      class: 'xxxx01',
+    },
+    {
+      id: 22,
+      class: 'xxxx01',
+    },
+    {
+      id: 23,
+      class: 'xxxx01',
+    },
 
+    {
+      id: 24,
+      class: 'xxxx01',
+    },
+    {
+      id: 25,
+      class: 'xxxx01',
+    },
+    {
+      id: 26,
+      class: 'xxxx01',
+    },
+    {
+      id: 27,
+      class: 'xxxx01',
+    },
+    {
+      id: 28,
+      class: 'xxxx01',
+    },
+    {
+      id: 29,
+      class: 'xxxx01',
+    },
+    {
+      id: 30,
+      class: 'xxxx01',
+    },
+    {
+      id: 31,
+      class: 'xxxx01',
+    },
 
-		{
-			id: 16,
-			class: 'xxxx01',
-		},
-		{
-			id: 17,
-			class: 'xxxx01',
-		},
-		{
-			id: 18,
-			class: 'xxxx01',
-		},
-		{
-			id: 19,
-			class: 'xxxx01',
-		},
-		{
-			id: 20,
-			class: 'xxxx01',
-		},
-		{
-			id: 21,
-			class: 'xxxx01',
-		},
-		{
-			id: 22,
-			class: 'xxxx01',
-		},
-		{
-			id: 23,
-			class: 'xxxx01',
-		},
+    {
+      id: 32,
+      class: 'xxxx01',
+    },
+    {
+      id: 33,
+      class: 'xxxx01',
+    },
+    {
+      id: 34,
+      class: 'xxxx01',
+    },
+    {
+      id: 35,
+      class: 'xxxx01',
+    },
+    {
+      id: 36,
+      class: 'xxxx01',
+    },
+    {
+      id: 37,
+      block: true,
+      class: 'xxxx02',
+    },
+    {
+      id: 38,
+      class: 'xxxx01',
+    },
+    {
+      id: 39,
+      class: 'xxxx01',
+    },
 
-
-
-		{
-			id: 24,
-			class: 'xxxx01',
-		},
-		{
-			id: 25,
-			class: 'xxxx01',
-		},
-		{
-			id: 26,
-			class: 'xxxx01',
-		},
-		{
-			id: 27,
-			class: 'xxxx01',
-		},
-		{
-			id: 28,
-			class: 'xxxx01',
-		},
-		{
-			id: 29,
-			class: 'xxxx01',
-		},
-		{
-			id: 30,
-			class: 'xxxx01',
-		},
-		{
-			id: 31,
-			class: 'xxxx01',
-		},
-
-
-
-		{
-			id: 32,
-			class: 'xxxx01',
-		},
-		{
-			id: 33,
-			class: 'xxxx01',
-		},
-		{
-			id: 34,
-			class: 'xxxx01',
-		},
-		{
-			id: 35,
-			class: 'xxxx01',
-		},
-		{
-			id: 36,
-			class: 'xxxx01',
-		},
-		{
-			id: 37,
-			block: true,
-			class: 'xxxx02',
-		},
-		{
-			id: 38,
-			class: 'xxxx01',
-		},
-		{
-			id: 39,
-			class: 'xxxx01',
-		},
-
-
-
-		{
-			id: 40,
-			class: 'xxxx01',
-		},
-		{
-			id: 41,
-			class: 'xxxx01',
-			treasure: true,
-		},
-		{
-			id: 42,
-			class: 'xxxx01',
-		},
-		{
-			id: 43,
-			class: 'xxxx01',
-		},
-		{
-			id: 44,
-			class: 'xxxx01',
-			mine: true,
-		},
-		{
-			id: 45,
-			class: 'xxxx01',
-			mine: true,
-		},
-		{
-			id: 46,
-			class: 'xxxx01',
-		},
-		{
-			id: 47,
-			class: 'xxxx01'
-		},
-	]
-}
+    {
+      id: 40,
+      class: 'xxxx01',
+    },
+    {
+      id: 41,
+      class: 'xxxx01',
+      treasure: true,
+    },
+    {
+      id: 42,
+      class: 'xxxx01',
+    },
+    {
+      id: 43,
+      class: 'xxxx01',
+    },
+    {
+      id: 44,
+      class: 'xxxx01',
+      mine: true,
+    },
+    {
+      id: 45,
+      class: 'xxxx01',
+      mine: true,
+    },
+    {
+      id: 46,
+      class: 'xxxx01',
+    },
+    {
+      id: 47,
+      class: 'xxxx01',
+    },
+  ],
+};
 export default game;
